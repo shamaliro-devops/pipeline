c3e610e19a63539d0a1dbdcf605fa45769b30b79
sha355 <shamaliroshan050@gmail.com>
2026-02-11 10:25:29 +0530

    initial commint

---
+++ src/main/java/com/ci/pipeline/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.ci.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/tests")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "Success!...";
+    }
+}
